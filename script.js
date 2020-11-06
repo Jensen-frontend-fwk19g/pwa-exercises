6d8814d28a726178d4625f76c9489f5ce38a5e82
David Andersson <lejonmanen@gmail.com>
2020-11-06 11:17:03 +0100

    Lade till script.js och länkade den från index.html

---
+++ script.js
@@ -0,0 +1,26 @@
+const items = [
+    {
+        name: 'Array length',
+        description: 'Get the length of an array',
+        code: 'let length = array.length;'
+    },
+    {
+        name: 'For each',
+        description: '',  // TODO
+        code: 'array.forEach( item => { /* do things with item */ } );'
+    }
+]
+
+console.log('JavaScript higher order functions');
+
+/*
+" citattecken, quotes "
+' apostrof, single quotes '
+` grav accent ` (template strings)
+´ akut accent ´ (används inte till strängar)
+* asterisk
+( parentes, parentheses/brackets/braces )
+[ hakparents, square brackets ]
+{ måsvingar/blockparentes, curly braces }
+
+*/
