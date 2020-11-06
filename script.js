5d97723a1f400209ee2ed5c94ce567730059170e
David Andersson <lejonmanen@gmail.com>
2020-11-06 14:03:49 +0100

    Övning 1.2.4

--- script.js
+++ script.js
@@ -29,13 +29,68 @@ const items = [
         description: 'Skapar en ny array genom att omvandla varje element i arrayen',
         code: `items.map(mystery => mystery.name)`
         // The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
-    }
+    },
+    {
+        name: '',
+        description: '',
+        code: ``
+    },
 ]
 
 console.log('JavaScript higher order functions');
 
 let a = items.find(mystery => mystery.name == 'Map');
-console.log('4a: a==', a);
+// console.log('4a: a==', a);
+
+let b = items.map(mystery => mystery.name);
+// console.log('4b: b==', b);
+
+// let nameAndCode = items.map(item => ({
+//     name: item.name,
+//     sample: item.code
+// }));
+// console.log('Name and code: ', nameAndCode);
+
+// Första elementet som saknar description
+let c = items.find(mystery => mystery.description == '');
+
+// Reducerar en array till ett värde
+// Lägger ihop alla koder till en sträng
+let d = items.reduce((acc, cur) => acc + '\n' + cur.code, '');
+// console.log('4d: d == ', d);
+
+// ... kallas "spread operator"
+// Skapar en ny lista med objekt som har allt som fanns i den ursprungliga listan, men en extra egenskap
+let e = items.map(mystery => ({ ...mystery, category: 'general' }))
+//                           ({ name, description, code, category })
+// console.log('4e: ', e);
+
+// Creates a new array with only the elements that the arrow function returns true for
+let f = items.filter(mystery => mystery.description != '');
+// console.log('4f', f);
+
+// Exempel: hitta alla action-filmer
+// let action = movies.filter(movie => movie.keyword == 'action')
+
+
+// Skapar en ny lista med alla element som har ett namn som kommer efter M i bokstavsordning
+let g = items.filter(mystery => {
+    // does not work if name===''
+    if( mystery.name == '' ) {
+        return true;
+    } else {
+        const c = mystery.name[0].toUpperCase();
+        return c > 'M';
+    }
+});
+console.log('4g: ', g);
+
+
+
+// "Ta bort" elementet med id==3 från arrayen
+const remove = 3;
+let h = items.filter(mystery => mystery.id != remove);
+
 
 
 /*
@@ -47,5 +102,7 @@ console.log('4a: a==', a);
 ( parentes, parentheses/brackets/braces )
 [ hakparents, square brackets ]
 { måsvingar/blockparentes, curly braces }
+: kolon
+; semikolon
 
 */
