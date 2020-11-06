38528f2a1261bd96785dd74d4f09cd4319b6c9d5
David Andersson <lejonmanen@gmail.com>
2020-11-06 11:44:31 +0100

    Övning 4a

--- script.js
+++ script.js
@@ -6,12 +6,37 @@ const items = [
     },
     {
         name: 'For each',
-        description: '',  // TODO
+        description: 'Calls a function once for each element in an array, in order',
         code: 'array.forEach( item => { /* do things with item */ } );'
+    },
+    {
+        name: 'document.querySelector',
+        description: 'Används för att hitta ett element i DOM, med hjälp av en CSS-selektor.',
+        code: `let button = document.querySelector('.form button');`
+    },
+    {
+        name: 'element.addEventListener',
+        description: 'Lägga på en händelse på elementet, t.ex ett klick',
+        code: `button.addEventListener('click', event => {})`
+    },
+    {
+        name: 'document.createElement',
+        description: 'Skapar upp ett element i DOM',
+        code: `let button = document.createElement('button');`
+    },
+    {
+        name: 'Map',
+        description: 'Skapar en ny array genom att omvandla varje element i arrayen',
+        code: `items.map(mystery => mystery.name)`
+        // The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
     }
 ]
 
 console.log('JavaScript higher order functions');
+
+let a = items.find(mystery => mystery.name == 'Map');
+console.log('4a: a==', a);
+
 
 /*
 " citattecken, quotes "
